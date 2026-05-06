c3e143f45277d8a308bd06d510d6ab911bc23a41
Jill <denissebauts@gmail.com>
2026-05-06 15:33:42 -0700

    Embedded main key parts involving this page

--- server.js
+++ server.js
@@ -7,8 +7,19 @@ const PORT = process.env.PORT || 3000;
 
 //Express files in views folder to render ejs
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(__dirname + "/public"));
+
+app.get("/game", (req, res) => {
+  res.render("game");
+});
+
+/*Temporary - for development purposes only,
+ will be removed or linked when question is wrong*/
+app.get("/gameincorrect", (req, res) => {
+  res.render("gameincorrect");
+});
 
 app.get("/profile", (req, res) => {
   res.sendFile(__dirname + "/public/profile.html");
