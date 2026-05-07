a4b1540893ed2666be7a54d816c1fab8ca204fa4
Jill <denissebauts@gmail.com>
2026-05-07 13:51:29 -0700

    Trying to connect to the database again

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ const mongodb_host = process.env.MONGODB_HOST;
 const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
 
-const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/`;
+const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}`;
 
 const client = new MongoClient(atlasURI);
 let db;
