25561d72f1d9e1d1353323b9f5317adbe15867d8
Varshini Sundari Babu <varshu895@gmail.com>
2026-05-07 20:13:15 -0700

    added tutorial video for the pop-up-challenge

---
+++ public/js/tutorial.js
@@ -0,0 +1,24 @@
+const modal = document.getElementById("videoModal");
+const openBtn = document.getElementById("tutorial-btn");
+const closeBtn = document.querySelector(".close-icon");
+const video = document.querySelector("video");
+
+// Open modal
+openBtn.addEventListener("click", () => {
+  modal.showModal(); // Use showModal() for backdrop
+  video.play();
+});
+
+// Close modal & pause video
+closeBtn.addEventListener("click", () => {
+  video.pause();
+  modal.close();
+});
+
+// Close when clicking outside
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    video.pause();
+    modal.close();
+  }
+});
